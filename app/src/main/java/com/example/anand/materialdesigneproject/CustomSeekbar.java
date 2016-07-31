df226af441da0550c0e9dae4ad2ed7aba7f6602d
AnandLilhar <anandlilhare69@gmail.com>
2016-07-31 18:49:32 +0530

    adding custom seek bar activity for creating custom bar

---
+++ app/src/main/java/com/example/anand/materialdesigneproject/CustomSeekbar.java
@@ -0,0 +1,9 @@
+package com.example.anand.materialdesigneproject;
+
+/**
+ * Created by anand on 31/7/16.
+ */
+public class CustomSeekbar {
+
+
+}
--- app/src/main/java/com/example/anand/materialdesigneproject/MainActivity.java
+++ app/src/main/java/com/example/anand/materialdesigneproject/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.anand.materialdesigneproject;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    Button mToolBarButton,mCustomSeekBarButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBarButton=(Button)findViewById(R.id.toolbar);
+        mCustomSeekBarButton=(Button)findViewById(R.id.customtoolbar);
+        mCustomSeekBarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mToolBarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
