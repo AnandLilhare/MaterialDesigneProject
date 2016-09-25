3af221963caf8cee2670c5bcc060c04ddf4e6b7b
AnandLilhar <anandlilhare69@gmail.com>
2016-09-25 19:25:25 +0530

    adding third party library of seek bar and slider :https://github.com/rey5137/Material/wiki/Slider

---
+++ app/src/main/java/com/example/anand/materialdesigneproject/ToolBarActivity.java
@@ -0,0 +1,15 @@
+package com.example.anand.materialdesigneproject;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToolBarActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+    }
+}
