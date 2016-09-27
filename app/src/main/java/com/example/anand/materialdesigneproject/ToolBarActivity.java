a4437c6499fec135b0fe8f88a29d1306f28467b6
AnandLilhar <anandlilhare69@gmail.com>
2016-09-27 10:14:36 +0530

    setup tool bar xml file and  getSupportActionBar in tha toolbar activity

--- app/src/main/java/com/example/anand/materialdesigneproject/ToolBarActivity.java
+++ app/src/main/java/com/example/anand/materialdesigneproject/ToolBarActivity.java
@@ -2,12 +2,30 @@ package com.example.anand.materialdesigneproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ToolBarActivity extends AppCompatActivity{
-
+    private   Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tool_bar);
+        toolbar=(Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+       // getMenuInflater().inflate(R.menu.main.menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
